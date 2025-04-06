a44f5907a1a6a0e9fc11fb48e64e15dc0f2fbcdb
minukusunilkumar1910 <minukkusunil425@gmail.com>
2025-04-06 18:47:27 +0530

    Adding Loading Effect to Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
+import { useEffect, useState } from "react";
 import Nav from "./Nav";
 import Hero from "./Hero";
 import About from "./About";
 import Skills from "./Skills";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import Loader from "./Loader";
+
 function App() {
-  let styles = { color: "red" };
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay (you can replace with actual check if needed)
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2s loading effect
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <section className="mainsection" id="mainsection">
-        <Nav />
-        <Hero />
-        <About />
-        <Skills/>
-        <Projects/>
-        <Contact/>
-      </section>
+      {loading ? (
+        <Loader />
+      ) : (
+        <section className="mainsection" id="mainsection">
+          <Nav />
+          <Hero />
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+        </section>
+      )}
     </>
   );
 }
