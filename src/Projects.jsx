eb209ce05219e27ac5d5e48b1900531e9039efb5
minukusunilkumar1910 <minukkusunil425@gmail.com>
2025-06-26 21:16:05 +0530

    projects 2 more added

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,9 @@
 import "./Projects.css";
 import proj1 from "./assets/wonderlust.jpg";
 import proj2 from "./assets/20623.jpg";
-function Projects() {
+import proj3 from "./assets/clglogin.jpg";
+import proj4 from "./assets/pdt4.avif";
+function Projects() {                       
   let details = {
     project1: {
       heading: `"WONDERLUST" A Business Travel Magazine`,
@@ -10,6 +12,14 @@ function Projects() {
     project2: {
       heading: `"WEATHER" in Different States using its API's`,
       categories: ["REACT", "MaterialUI", "vite", "API's"],
+    },
+       project3: {
+      heading: `Smart Attendance Tracker`,
+      categories: ["REACT","bycript", "Express", "MongoDB"],
+    },
+       project4: {
+      heading: `"PeriodicPro"`,
+      categories: ["REACT", "Express", "vite", "tailwind", ],
     },
   };
 
@@ -47,6 +57,16 @@ function Projects() {
           details={details.project2.heading}
           img={proj2}
           project={details.project2.categories}
+        />
+        <Card
+          details={details.project3.heading}
+          img={proj3}
+          project={details.project3.categories}
+        />
+        <Card
+          details={details.project4.heading}
+          img={proj4}
+          project={details.project4.categories}
         />
         
       </div>
